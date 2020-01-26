1c4810b05800d5de7d7655bf2cdff29615f80565
unknown <mister.bouhmid@gmail.com>
2020-01-26 16:02:02 +0100

    Auth Gard Ok, Redirect KO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { SigninComponent } from './auth/signin/signin.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { SingleBookComponent } from './book-list/single-book/single-book.component';
 import { BookFormComponent } from './book-list/book-form/book-form.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 
 
@@ -11,9 +12,11 @@ import { BookFormComponent } from './book-list/book-form/book-form.component';
 const appRoutes: Routes = [
   { path: 'auth/signup', component: SignupComponent },
   { path: 'auth/signin', component: SigninComponent },
-  { path: 'books', component: BookListComponent },
+  { path: 'books', component: BookListComponent, canActivate :[AuthGuardService] },
   { path: 'books/new', component: BookFormComponent},
-  { path: 'books/view/:id', component: SingleBookComponent} 
+  { path: 'books/view/:id', component: SingleBookComponent},
+  { path : '', redirectTo: 'books', pathMatch: 'full'},
+  { path : '**', redirectTo: 'books'} // path wildcard 
 ];
 
 @NgModule({
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    router: RouterStateSnapshot
+  ): boolean 
+  | Promise<boolean | UrlTree> 
+  | Observable<boolean | UrlTree>
+  | UrlTree {
+    return this.authService.user.pipe(
+     take(1), // subsribe to the first value and then unsubscribe:
+     map(user => { 
+      const isAuth = !!user // !! convert truesh to boolean true, and falsech like undefined, null to false   
+      if (isAuth) { 
+        return true
+      }
+      return this.router.createUrlTree(['/auth/signin']);
+    }));    
+  }
+  constructor(private authService: AuthService, private router : Router) { }
 }
